5de4ad79c7804aaf20d2a2c13cefc36951f3f5b8
MatanLed <matan.harel.mh@gmail.com>
2018-04-14 17:04:06 +0300

    myLog works

--- ex1/NewtonRaphson.m
+++ ex1/NewtonRaphson.m
@@ -2,10 +2,10 @@ function [zero, Niters]=NewtonRaphson(func, functag, tol, a, b)
     Niters = 0;
     zero = a + (b-a)*rand(1,1); % start at random position inside (a,b)
     last = b + tol;
-    while(func(zero) ~=0 && abs(zero - last) > tol)
+    while func(zero) ~=0 && abs(zero - last) > tol
         last = zero;
         Niters = Niters+1;
-        if(functag(zero) == 0) %if derivative is zero find a new start point:
+        if functag(zero) == 0 %if derivative is zero find a new start point:
             zero = a + (b-a)*rand(1,1);
         else
             zero = zero - func(zero)/functag(zero);
@@ -13,17 +13,17 @@ function [zero, Niters]=NewtonRaphson(func, functag, tol, a, b)
         
         %if zero is outside (a, b) run an bisection for on
         %iteration:
-        if(zero < a || zero >b) 
+        if zero < a || zero >b 
             %fprintf('out!\n');
             midPointValue = func((a+b)/2);
-            if (midPointValue>0)
-                if(func(a)>0)
+            if midPointValue>0
+                if func(a)>0
                     a = (a+b)/2;
                 else
                     b = (a+b)/2;
                 end
             else
-                if(func(a)>0)
+                if func(a)>0
                     b = (a+b)/2;
                 else
                     a = (a+b)/2;
--- ex1/myLog.m
+++ ex1/myLog.m
@@ -1,3 +1,44 @@
-function y=myLog(a,x)
-y = log(a,x)
+function y=myLog(a,x) %assuming a > 0, a != 1, x>12
+assert(a>0 && a~=1, 'a is illegal  value')
+assert(x>0, 'x is illegal  value')
+
+tol = 10^-10;
+lowerBound = -1;
+if(a < 1)
+    lowerBound = 1;
+end
+upperBound = -lowerBound;
+
+while a^lowerBound > x %find lower bound
+    lowerBound = lowerBound*2; 
+end
+while a^upperBound < x % find upper bound:
+    upperBound = upperBound*2;
+end
+
+%find b and k such as abs(b-1) < 1 and ln(a)=2^-k*ln(b)
+b = a;
+k = -1;
+while(abs(b-1) >= 1)
+    k = k*2;
+    b = a^(2^k);
+end
+b = b-1;
+
+%calc logb (works just for abs(b)<1)
+logb = b-b/2+b^2/3-b^3/4;
+ii=4;
+while abs(b^ii) > tol
+    if mod(ii,2) == 0
+        logb = logb + b^ii/(ii+1);
+    else
+        logb = logb - b^ii/(ii+1);
+    end
+    ii = ii+1;
+end
+
+%get the final results:
+loga = 2^-k * logb;
+[res, ] = NewtonRaphson(@(z)a^z-x,@(z)a^z*loga,tol,lowerBound,upperBound);
+y = res;
 end
