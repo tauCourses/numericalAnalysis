bb28910a8e892c9ce93fd52c7dff68d9755f1092
MatanLed <matan.harel.mh@gmail.com>
2018-05-16 23:02:14 +0300

    second version

--- ex2/HermiteShape.m
+++ ex2/HermiteShape.m
@@ -3,21 +3,23 @@ function [yy] = HermiteShape(x, y, ytag, ser)
     if isempty(cache)
         cache = containers.Map;
     end
-    
-    key = ser(1)*65536 + ser(end);
-    
-    if isKey(cache,ser)
-        yy = cache(ser);
-        return;
-    end
-    
     if length(ser) == 1
         yy = y(ser);
+        return
     elseif length(ser) == 2 && ser(1) == ser(2)
-        yy = ytag(ser(1));    
-    else
-        yy = HermiteShape(x, y, ytag, ser(2:end)) - HermiteShape(x, y, ytag, ser(1:end-1));
-        yy = yy / (x(ser(end)) - x(ser(1)));
+        yy = ytag(ser(1));
+        return
     end
-    cache(ser) = yy;
+    
+    key = char(ser);
+    
+    if isKey(cache,key)
+        yy = cache(key);
+        return;
+    end
+     
+    yy = HermiteShape(x, y, ytag, ser(2:end)) - HermiteShape(x, y, ytag, ser(1:end-1));
+    yy = yy / (x(ser(end)) - x(ser(1)));
+    
+    cache(key) = yy;
 end
---
+++ ex2/myLS.m
@@ -0,0 +1,18 @@
+function [yy] = myLS(x, y, xx, n)
+    X = zeros(length(x), n);
+    xt = x';
+    multi = ones(length(x),1);
+    for ii = 1:n+1
+        X(:,ii) = multi;
+        multi = multi .* xt;
+    end
+    
+    coefficients = X\y'; 
+    xxt = xx';
+    multi = ones(length(xx),1);
+    yy = zeros(length(xx),1);
+    for ii = 1:n+1
+        yy = yy + coefficients(ii)*multi;
+        multi = multi .* xxt;
+    end
+end
